d670bfe0bc59859b64da870cb1b67f987caee930
rei-bp <reinard.bp@gmail.com>
2021-05-17 11:36:34 -0700

    fix boat border collision

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //CANVAS ATTEMPT
 let canvas = document.getElementById('myCanvas')
 let ctx = canvas.getContext('2d')
-let x = canvas.width/2;
-let y = canvas.height-30;
-let rectRadius = 10;
+let x = canvas.width/2
+let y = canvas.height-30
+let splashX = 0
+let splashY = 0
+let splashRadius = 5
+let rectRadius = 10
 let rightPressed = false
 let leftPressed = false
 let upPressed = false
@@ -12,58 +15,69 @@ let downPressed = false
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", keyUpHandler, false)
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
+    if(e.key == "ArrowRight") {
+        rightPressed = true 
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
+    else if(e.key == "ArrowLeft" && x > 0) {
+        leftPressed = true
     }
-    else if(e.key == "Up" || e.key == "ArrowUp") {
-        upPressed = true;
+    else if(e.key == "ArrowUp" && y > 0) {
+        upPressed = true
     }
-    else if(e.key == "Down" || e.key == "ArrowDown") {
-        downPressed = true;
+    else if(e.key == "ArrowDown" && y < canvas.height) {
+        downPressed = true
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
+    if(e.key == "ArrowRight") {
+         rightPressed = false
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
+    else if(e.key == "ArrowLeft") {
+        leftPressed = false
     }
-    else if(e.key == "Up" || e.key == "ArrowUp") {
-        upPressed = false;
+    else if(e.key == "ArrowUp") {
+        upPressed = false
     }
-    else if(e.key == "Down" || e.key == "ArrowDown") {
-        downPressed = false;
+    else if(e.key == "ArrowDown") {
+        downPressed = false
     }
 }
 
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function boat () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath()
     ctx.rect(x, y, rectRadius, 10)
     ctx.fillStyle = "#ff000"
     ctx.fill()
     ctx.closePath()
     if(rightPressed) {
-        x += 1;
-    }
-    else if(leftPressed) {
-        x -= 1;
+        x += 1
+        } else if (x > canvas.width) {
+            x = 0
+    } else if(leftPressed) {
+        x -= 1
+        } else if (x < 0) {
+            x = canvas.width
     }
     else if(upPressed) {
-        y -= 1;
+        y -= 1
+        } else if ( y < 0) {
+            y = canvas.height
     }
     else if(downPressed) {
-        y += 1;
-    }
-    
+        y += 1
+        } else if ( y > canvas.height) {
+            y = 0            
+        }
 }
 
+
+function draw() {
+    boat ()
+    }
+    
+    
 
 setInterval(draw, 10)
 
