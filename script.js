38dce39ad3fbdc7b5a7eb7a801e6ca6a1d633914
rei-bp <reinard.bp@gmail.com>
2021-05-19 13:42:21 -0700

    start styling/displaying data

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 //CANVAS ATTEMPT
+//global vars
 let canvas = document.getElementById('myCanvas')
 let ctx = canvas.getContext('2d')
-// let x = canvas.width/2
-// let y = canvas.height-30
-// let splashX = 10
-// let splashY = 10
-// let splashRadius = 5
 let boat
 let splash
 let bag = []
 let space = false
+
+
+//global functions
 function randomWeight() {
     return Math.floor(Math.random()*(45-5)+5)
 }
@@ -18,19 +17,11 @@ function randomSplashGenerator () {
     splash.x = Math.floor(Math.random()*795)
     splash.y = Math.floor(Math.random()*595)
 }
-// let rectRadius = 10
-// let rightPressed = false
-// let leftPressed = false
-// let upPressed = false
-// let downPressed = false
 
-// function calcCir (radius) {
-//     let circumference = ((2*Math.PI) * radius)
-//     return circumference
-// }
+
 function getDistance(x1,y1,x2,y2) {
-    let xDistance = x2 - x1;
-    let yDistance = y2- y1;
+    let xDistance = x2 - x1
+    let yDistance = y2- y1
     return Math.sqrt(Math.pow(xDistance, 2) + 
         Math.pow(yDistance,2)
     )
@@ -116,14 +107,14 @@ function Splash () {
 
     }
     this.update = function () {
-        this.draw();
+        this.draw()
     }
 }
 
 function Boat () {
     this.rectRadius = 10
-    this.x = canvas.width/2;
-    this.y = canvas.height-30;
+    this.x = canvas.width/2
+    this.y = canvas.height-30
     this.rightPressed = false
     this.leftPressed = false
     this.upPressed = false
@@ -142,28 +133,28 @@ function Boat () {
     }
     this.movementSpeed = function() {
         if(this.rightPressed) {
-            this.x += 1
+            this.x += 3
         } if (this.x > canvas.width) {
             this.x = 0
         } if(this.leftPressed) {
-            this.x -= 1
+            this.x -= 3
         } if (this.x < 0) {
             this.x = canvas.width
         }
         if(this.upPressed) {
-            this.y -= 1
+            this.y -= 3
         } if ( this.y < 0) {
             this.y = canvas.height
         }
         if(this.downPressed) {
-            this.y += 1
+            this.y += 3
         } if ( this.y > canvas.height) {
             this.y = 0            
         }
     }
     this.update = function () {
-        this.draw();
-        this.movementSpeed();    
+        this.draw()
+        this.movementSpeed()
     }
 }
 
@@ -184,7 +175,7 @@ const animate = setInterval(function(){
     } else {
         boat.fishing = false
     }
-}, 10)
+}, 20)
 
 // const splashAnimate = setInterval(function(){
 //     splash.x = Math.floor(Math.random()*795)
@@ -195,41 +186,3 @@ const animate = setInterval(function(){
 
 init();
 
-// document.addEventListener('DOMContentLoaded', function () {
-    
-//     let boat = document.getElementsByClassName('boat')
-
-//     let moveUp = () => {
-//         boat.style.top = parseInt(boat.style.top) - 5 + 'px'
-//     } 
-//     let moveRight = () => {
-//         boat.style.right = parseInt(boat.style.right) - 5 + 'px'
-//     } 
-//     let moveDown = () => {
-//         boat.style.top = parseInt(boat.style.top) + 5 + 'px'
-//     } 
-//     let moveLeft = () => {
-//         boat.style.right = parseInt(boat.style.right) + 5 + 'px'
-//     }
-    
-
-//     boat.addEventListener('keydown', (e) => {
-//             e.preventdefault() 
-//         let movementHandler = e => {
-//             switch (e.keyCode) {
-//                 case (87):
-//                 moveUp()
-//                 break
-//                 case (68):
-//                 moveRight()
-//                 break
-//                 case (83):
-//                 moveDown()
-//                 break
-//                 case (65):
-//                 moveLeft()
-//                 break
-//             } 
-//         }
-//     })        
-// })
